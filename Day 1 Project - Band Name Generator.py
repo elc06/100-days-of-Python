8665fd2669390d4952f137190af1dd5e058454d1
elc006 <elsa.chang006@gmail.com>
2025-06-06 14:41:16 -0700

    Initial commit: Day 1

---
+++ Day 1 Project - Band Name Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+x = input("What's the name of your pet?\n")
+y = input("What's the funny phrase you want your band's name to have?\n")
+print("Your band's name could be " + x + " " + y)
